09b767bd8634159a2e2487008e84cf85a80efe48
John <johnathanlee72@hotmail.com>
2022-04-18 14:35:30 -0700

    Initial Commit

---
+++ CardUIChallenge/CardUIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardUIChallengeApp.swift
+//  CardUIChallenge
+//
+//  Created by Johnathan Lee on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct CardUIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
