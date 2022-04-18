d9426f3e583697cdb0434f5f5a9fb5ce3be6bcc3
John <johnathanlee72@hotmail.com>
2022-04-18 14:56:07 -0700

    - Initial commit - Create cards with random colors

--- CardUIChallenge/ContentView.swift
+++ CardUIChallenge/ContentView.swift
@@ -9,8 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        GeometryReader {
+            geo in
+            
+            TabView {
+                ForEach(0..<50) {
+                    numIndex in
+                    
+                    let redNum = Double.random(in: 0...1)
+                    let greenNum = Double.random(in: 0...1)
+                    let blueNum = Double.random(in: 0...1)
+                    let randomColor = Color(.sRGB, red: redNum, green: greenNum, blue: blueNum, opacity: 1)
+                    
+                    ZStack {
+                        Rectangle()
+                            .frame(width: geo.size.width*0.9, height: geo.size.height*0.9, alignment: .center)
+                            .foregroundColor(randomColor)
+                            .cornerRadius(20)
+                            .shadow(color: .black, radius: 10, x: 5, y: 5)
+                        
+                        Circle()
+                            .frame(width: geo.size.width*0.8, height: geo.size.height*0.8, alignment: .center)
+                            .padding(.all)
+                        
+                        Circle()
+                            .frame(width: geo.size.width*0.65, height: geo.size.height*0.65, alignment: .center)
+                            .padding(.all)
+                            .foregroundColor(randomColor)
+                        
+                        Text("\(numIndex)")
+                            .font(.system(size: geo.size.width*0.3))
+                            .fontWeight(.bold)
+                        
+                    }
+                }
+                
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+        }
     }
 }
 
